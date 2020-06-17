3cdfc8192950ec6a3318845e954affdda0214329
Sudhanshu Ghinmine <sghinmine54@gmail.com>
2020-06-17 21:35:24 +0530

    STARTED address book with welcome message

---
+++ src/com/su481ns8u/AddressBook/AddressBookMain.java
@@ -0,0 +1,7 @@
+package com.su481ns8u.AddressBook;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book !");
+    }
+}
