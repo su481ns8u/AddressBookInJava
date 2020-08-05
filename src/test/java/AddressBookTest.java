3b7bc2122fbf1ee93d58f8d41443da69b2e2925f
Sudhanshu Ghinmine <sghinmine54@gmail.com>
2020-08-05 10:14:17 +0530

    ADDED Test to test api using rest assured

--- src/test/java/AddressBookTest.java
+++ src/test/java/AddressBookTest.java
@@ -1,38 +1,52 @@
-//import io.restassured.RestAssured;
-//import io.restassured.response.Response;
-//import io.restassured.specification.RequestSpecification;
-//import org.json.simple.JSONObject;
-//import org.junit.Assert;
-//import org.junit.Test;
-//
-//public class AddressBookTest {
-//    @Test
-//    public void restAssuredPutTest() {
-//        RequestSpecification requestSpecification = RestAssured.given();
-//        requestSpecification.header("Content-Type", "application/json");
-//
-//        JSONObject jsonObject = new JSONObject();
-//        jsonObject.put("Mobile No.", "91 985564856");
-//        jsonObject.put("Zip", "465231");
-//        jsonObject.put("Address", "Harsh");
-//        jsonObject.put("State", "Maha");
-//        jsonObject.put("First Name", "");
-//        jsonObject.put("City", "Sams");
-//        jsonObject.put("Last Name", "Ghinm");
-//
-//        JSONObject personObject = new JSONObject();
-//        personObject.put("Person", jsonObject);
-//
-//        requestSpecification.body(personObject.toJSONString());
-//        Response response = requestSpecification.post("http://localhost:3000/Person");
-//        int code = response.getStatusCode();
-//        Assert.assertEquals(code, 201);
-//    }
-//
-//    @Test
-//    public void name() {
-//        RequestSpecification requestSpecification = RestAssured.given();
-//        Response response = requestSpecification.get("http://localhost:3000/Person");
-//        System.out.println(response);
-//    }
-//}
+import io.restassured.http.ContentType;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+
+public class AddressBookTest {
+    @Test
+    public void givenIDAndURLOfApi_UsingRestAssured_ShouldReturnPassingStatusCode() {
+        int statusCode = given().queryParam("id", 1)
+                .when()
+                .get("https://my-json-server.typicode.com/su481ns8u/AddressBookSampleDB/person")
+                .getStatusCode();
+        Assert.assertEquals(200, statusCode);
+    }
+
+    @Test
+    public void givenJSonStringToPost_UsingRestAssured_ShouldReturnPassingResponseStatus() {
+        given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body("{\"FirstName\": \"Shashank\", \"LastName\": \"Ghinmine\", \"Mobile\": \"91 9822917991\", " +
+                        "\"Address\": \"Kesha\", \"City\": \"Selu\",\"State\": \"Maha\",\"Zip\": \"465231\"}")
+                .when()
+                .post("https://my-json-server.typicode.com/su481ns8u/AddressBookSampleDB/person").then()
+                .statusCode(201)
+                .log().all();
+    }
+
+    @Test
+    public void givenJsonStringToUpdate_UsingRestAssured_ShouldReturnPassingStatus() {
+        given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body("{\"FirstName\": \"Shashank\", \"LastName\": \"Ghinmine\", \"Mobile\": \"91 9822917991\", " +
+                        "\"Address\": \"Kesha\", \"City\": \"Selu\",\"State\": \"Maha\",\"Zip\": \"465231\"}")
+                .when()
+                .put("https://my-json-server.typicode.com/su481ns8u/AddressBookSampleDB/person/1").then()
+                .statusCode(200)
+                .log().all();
+    }
+
+    @Test
+    public void givenURLToDelete_UsingRestAssured_ShouldReturnPassingStatus() {
+        when()
+                .delete("https://my-json-server.typicode.com/su481ns8u/AddressBookSampleDB/person/1")
+                .then()
+                .statusCode(200)
+                .log().all();
+    }
+}
